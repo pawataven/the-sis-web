5353c00374c5d143a63c8aad4170799438d33406
pawataven <pawatwantrong@gmail.com>
2026-05-27 04:32:02 +0700

    ดเดเดเ

--- app/data/Flower/flowerData.ts
+++ app/data/Flower/flowerData.ts
@@ -332,7 +332,7 @@ export const flowerList: Flower[] = [
     {
     id: "violet",
     name: "Violet",
-    titleEn: "violetด",
+    titleEn: "violet",
     titleTh: "ดอกไวโอเล็ต",
     image: "/flowers/popupImg/Violet.png",
     description: "ความสุภาพถ่อมตน<br>(Modesty)",
